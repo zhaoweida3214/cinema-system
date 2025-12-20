1cebcd0c0484ac4a1a77f7ad5351bb57d7990ffa
xyj <1279111200@@qq.com>
2025-12-20 14:52:37 +0800

    初始代码（测试一下）

---
+++ src/main/java/common/Result.java
@@ -0,0 +1,29 @@
+package common;
+import lombok.Data;
+import java.io.Serializable;
+
+@Data
+public class Result<T> implements Serializable {
+    private Integer code; // 1: 成功, 0: 失败
+    private String msg;
+    private T data;
+
+    public static <T> Result<T> success() {
+        return success(null);
+    }
+
+    public static <T> Result<T> success(T data) {
+        Result<T> result = new Result<>();
+        result.code = 1;
+        result.msg = "success";
+        result.data = data;
+        return result;
+    }
+
+    public static <T> Result<T> error(String msg) {
+        Result<T> result = new Result<>();
+        result.code = 0;
+        result.msg = msg;
+        return result;
+    }
+}
